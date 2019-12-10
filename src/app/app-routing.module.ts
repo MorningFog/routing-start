00b3c02797885fb9826383892cc68c358b105c6b
MorningFog <veronag@me.com>
2019-12-10 13:00:17 +0000

    Last pieces completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -23,16 +26,18 @@ const appRoutes: Routes = [
         // canActivate: [AuthGuard],
         canActivateChild: [AuthGuard],
         component: ServersComponent, children: [
-            { path: ':id', component: ServerComponent },
-            { path: ':id/edit', component: EditServerComponent }
+            { path: ':id', component: ServerComponent, resolve: { server: ServerResolver } },
+            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
         ]
     },
-    { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: PageNotFoundComponent },
+    { path: 'not-found', component: ErrorPageComponent, data: { message: 'Page Not Found!' } },
     { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
     imports: [
+        // RouterModule.forRoot(appRoutes, { useHash: true })
         RouterModule.forRoot(appRoutes)
     ],
     exports: [
